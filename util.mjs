4b37c928a644ea5b2e0fee0bd71187d269bc3455
MassHesteria <brad.hester@gmail.com>
2023-04-23 15:42:13 -0500

    Added a few rooms

---
+++ util.mjs
@@ -0,0 +1,48 @@
+import data from "./vertex.json" assert { type: "json" };
+import table from "table";
+
+const vertices = data.vertices;
+
+export const con = (from, to, condition = () => true) => {
+  const a = vertices.find((v) => v.name == from);
+  const b = vertices.find((v) => v.name == to);
+  return {
+    from: a,
+    to: b,
+    condition: condition,
+  };
+};
+
+export const printMap = (map) => {
+  const name = (vertex) => {
+    if (vertex == undefined) {
+      return "(invalid name)";
+    } else {
+      return vertex.name;
+    }
+  };
+  const conToString = (connection) => {
+    return (
+      name(connection.from) +
+      " to " +
+      name(connection.to) +
+      "\n\n" +
+      connection.condition.toString()
+    );
+  };
+
+  const tableData = [];
+  for (let i = 0; i < map.length; i += 2) {
+    tableData.push([conToString(map[i]), conToString(map[i + 1])]);
+  }
+
+  console.log(table.table(tableData));
+};
+
+export const printAvailable = (map, samus) => {
+  const queue = [];
+
+  const first = map[0];
+
+  for (let i = 1; i < map.length; i++) {}
+};
---
+++ vanilla.mjs
@@ -0,0 +1,33 @@
+import { con, printMap } from "./util.mjs";
+
+let vanilla = [
+  /* ----- Landing Site ----- */
+
+  con("LandingSite_Ship", "LandingSite_Ledge", (samus) => samus.canFly),
+  con("LandingSite_Ledge", "LandingSite_Ship"),
+
+  con("LandingSite_Ship", "CrateriaTube", (samus) => samus.hasSupers),
+  con("CrateriaTube", "LandingSite_Ship"),
+
+  con(
+    "LandingSite_Ship",
+    "LandingSite_PreGauntlet",
+    (samus) => samus.canUseBombs || samus.canUsePowerBombs
+  ),
+  con(
+    "LandingSite_PreGauntlet",
+    "LandingSite_Ship",
+    (samus) => samus.canUseBombs || samus.canUsePowerBombs
+  ),
+
+  con("LandingSite_Ledge", "LandingSitePBs", (samus) => samus.canUsePowerBombs),
+  con("LandingSitePBs", "LandingSite_Ledge"),
+
+  con("LandingSite_Ship", "Parlor_Main"),
+  con("Parlor_Main", "LandingSite_Ship"),
+
+  con("Parlor_Main", "Parlor_Alcatraz", (samus) => samus.hasMorph),
+  con("Parlor_Alcatraz", "Parlor_Main", (samus) => samus.hasMorph),
+];
+
+printMap(vanilla);
