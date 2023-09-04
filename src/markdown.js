3391a42834f8155eb2af1a74a7f07cea4633c74b
MassHesteria <brad.hester@gmail.com>
2023-09-04 09:35:50 -0500

    Uses prettier for MD logic

--- src/markdown.js
+++ src/markdown.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { loadGraph } from "./lib/graph/init";
 import { MajorDistributionMode, MapLayout } from "./lib/graph/params";
+import * as prettier from "prettier";
 
 const getBody = (func) => {
   const full = func.toString();
@@ -11,36 +12,43 @@ const getBody = (func) => {
     .slice(full.indexOf("{") + 1, full.lastIndexOf("}"))
     .replace(/return /g, "")
     .replace(/;/g, "")
+    .replace(/\/\/.*/g, "")
+    .replace(/[\n\r]/g, "")
     .trim();
 };
 
 let area = "";
 
-const writeEdge = (e) => {
+const writeEdge = async (e) => {
   let output = "";
   if (e.from.area != area) {
-    output += `# ${e.from.area}   \n\n`;
+    output += `# ${e.from.area}   \n   \n`;
     area = e.from.area;
   }
-  output +=
-    `From **${e.from.name}** to **${e.to.name}**   \n` +
-    "```\n" +
-    `${getBody(e.condition)}` +
-    "\n```\n";
+  let condition = await prettier.format(getBody(e.condition), { semi: false, parser: "babel" });
+  if (condition[0] == ";") {
+    condition = condition.slice(1);
+  }
+  output += `From **${e.from.name}** to **${e.to.name}**   \n` + "```\n" + `${condition}` + "```\n";
   return output;
 };
 
-const writeGraph = (name, graph) => {
+const writeGraph = async (name, graph) => {
   let msg = "";
-  graph.slice(0, graph.length - 40).forEach((e) => (msg += writeEdge(e)));
+  for (let i = 0; i < graph.length - 40; i++) {
+    msg += await writeEdge(graph[i]);
+  }
   fs.writeFileSync(`${name}.md`, msg);
 };
 
-writeGraph("standard", loadGraph(1, 1, MapLayout.Standard, MajorDistributionMode.Standard));
-writeGraph(
+await writeGraph("standard", loadGraph(1, 1, MapLayout.Standard, MajorDistributionMode.Standard));
+await writeGraph(
   "standard-area",
   loadGraph(1, 1, MapLayout.Standard, MajorDistributionMode.Standard, true)
 );
-writeGraph("recall", loadGraph(1, 1, MapLayout.Recall, MajorDistributionMode.Recall));
-writeGraph("recall-area", loadGraph(1, 1, MapLayout.Recall, MajorDistributionMode.Recall, true));
+await writeGraph("recall", loadGraph(1, 1, MapLayout.Recall, MajorDistributionMode.Recall));
+await writeGraph(
+  "recall-area",
+  loadGraph(1, 1, MapLayout.Recall, MajorDistributionMode.Recall, true)
+);
 process.exit(0);
